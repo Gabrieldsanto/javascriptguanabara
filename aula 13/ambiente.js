d2ecf4085776b04f35bb1a9c26381e9f12116111
Gabriel <santoscostagabriel@hotmail.com>
2022-11-22 22:38:01 -0300

    Fiz as aulas de Repetiçoes em JS

---
+++ aula 13/ambiente.js
@@ -0,0 +1,33 @@
+
+var c = 1
+
+do{
+    console.log('Olá. como vai?')
+    c++ // c++ é igual a c = c +1
+}while (c <= 8)
+
+
+/*
+var d = 1
+
+while(d <=5){
+    console.log(`Passo ${d}`)
+     d++
+}
+
+*/
+
+
+
+
+
+
+/*
+console.log('Olá. Tudo bem?')
+console.log('Olá. Tudo bem?')
+console.log('Olá. Tudo bem?')
+console.log('Olá. Tudo bem?')
+console.log('Olá. Tudo bem?')
+console.log('Olá. Tudo bem?')
+*/
+
